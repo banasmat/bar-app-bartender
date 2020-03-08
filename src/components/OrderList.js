f7a3c6a2b86328fa8d1ea035a0ab77dfb25832ef
Mateusz Banaś <m.banas@createit.pl>
2020-03-08 15:39:02 +0100

    Basic orders list

--- src/components/OrderList.js
+++ src/components/OrderList.js
@@ -9,10 +9,17 @@ import Divider from '@material-ui/core/Divider';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Avatar from '@material-ui/core/Avatar';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import {PLACE_ID} from "../constants/config";
 
 const useStyles = makeStyles(theme => ({
@@ -21,6 +28,18 @@ const useStyles = makeStyles(theme => ({
         display: 'flex',
         alignItems: 'center',
     },
+    table: {
+        borderCollapse: 'initial',
+    },
+    status_2: {
+        backgroundColor: 'yellow'
+    },
+    status_3: {
+        backgroundColor: 'blue'
+    },
+    status_4: {
+        backgroundColor: 'green'
+    }
 }));
 
 const mapStateToProps = state => {
@@ -33,14 +52,70 @@ const OrderList = ({ orders, getOrderData }) => {
 
     useEffect(() => {
         getOrderData(PLACE_ID);
-    });
+        setInterval(()=>{
+            getOrderData(PLACE_ID);
+        }, 20000);
+
+    }, []);
 
     return (
         <Box className={classes.root}>
             <List>
                 {orders.map((order) => {
+
                     return (<ListItem key={order.id} alignItems="flex-start">
-                            {order.id}
+                            <ExpansionPanel>
+                                <ExpansionPanelSummary
+                                    className={classes["status_"+order.status]}
+                                    expandIcon={<ExpandMoreIcon />}
+                                    aria-controls="panel1a-content"
+                                    id="panel1a-header"
+                                >
+                                    {/*TODO later user's image from his profile */}
+                                    {/*<ListItemAvatar>*/}
+                                    {/*    <Avatar alt={menuItem.name} />*/}
+                                    {/*</ListItemAvatar>*/}
+                                    <ListItemText
+                                        primary={order.createdAt} //TODO user's name
+                                        secondary={
+                                            <React.Fragment>
+                                                <Typography
+                                                    component="span"
+                                                    variant="body2"
+                                                    color="textPrimary"
+                                                >
+                                                    ID: {order.id}
+                                                </Typography>
+                                                <Divider/>
+                                                <Typography
+                                                    component="span"
+                                                    variant="body2"
+                                                    color="textPrimary"
+                                                >
+                                                </Typography>
+                                                {/*TODO price */}
+
+                                            </React.Fragment>
+                                        }
+                                    />
+                                {/* TODO status buttons */}
+                                </ExpansionPanelSummary>
+                                <ExpansionPanelDetails>
+                                    <TableContainer component={Paper}>
+                                        <Table className={classes.table} aria-label="simple table">
+                                            <TableBody>
+                                                {order.data.map(orderItem => {
+                                                    return (
+                                                        <TableRow>
+                                                            <TableCell align="left" colSpan={2}>{orderItem.menuItem.name}</TableCell>
+                                                            <TableCell align="right">x{orderItem.count}</TableCell>
+                                                        </TableRow>
+                                                    )})}
+                                            </TableBody>
+                                        </Table>
+                                    </TableContainer>
+                                </ExpansionPanelDetails>
+                            </ExpansionPanel>
                         </ListItem>
                     )
                 })}
--- src/constants/config.js
+++ src/constants/config.js
@@ -1,1 +1,1 @@
-export const PLACE_ID = 'abc';
+export const PLACE_ID = '7e77e904-60bc-46a6-aeb6-44fa6d54cdf8';
