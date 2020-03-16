ceea9d0f8776c3e05211f0cde270b8ec8ea40f98
Mateusz Banaś <m.banas@createit.pl>
2020-03-16 21:41:41 +0100

    Set urls to prod

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,3 +1,4 @@
-export const API_BASE = 'http://localhost:8000';
+// export const API_BASE = 'http://localhost:8000';
+export const API_BASE = 'https://bar-app-api.banasmatgmailcom.smarthost.pl';
 export const GET_ORDERS = API_BASE + "/orders/{placeId}";
 export const ORDER_STATUS = API_BASE + "/order/{orderId}";
